bcc0be92d93b9a97dbc6ec89e7a074651f2a8018
Kaumil Patel <79810862+Kal0511@users.noreply.github.com>
2023-11-22 15:44:53 -0700

    Remove dotenv & convert js to ts

--- YYCBeeswaxMobileApplication/app/index.tsx
+++ YYCBeeswaxMobileApplication/app/index.tsx
@@ -1,10 +1,14 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+import useAuth from "@/firebase/hooks/useAuth";
 
 export default function App() {
+    const {user} = useAuth()
+
     return (
         <View style={styles.container}>
             <Text>Hello World</Text>
+            <Text>{user?.uid || 'Not Logged In'}</Text>
             <StatusBar style="auto"/>
         </View>
     );
--- YYCBeeswaxMobileApplication/babel.config.js
+++ YYCBeeswaxMobileApplication/babel.config.js
@@ -2,16 +2,6 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: [
-            'expo-router/babel',
-            [
-                "module:react-native-dotenv", 
-                {
-                    "envName": "APP_ENV",
-                    "moduleName": "@env",
-                    "path": ".env",
-                }
-            ],
-        ],
+        plugins: ['expo-router/babel',],
     };
 };
