d089c6ab90dc67ffb528387572095c7004c77795
Colin-C32 <colin.christophe@ucalgary.ca>
2023-11-28 21:57:18 -0700

    notifications

--- YYCBeeswaxMobileApplication/app/dashboard/notificationsPage.tsx
+++ YYCBeeswaxMobileApplication/app/dashboard/notificationsPage.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 import { mainStyles } from '@/styles/mainStyles';
 import React from 'react';
 
-export default function ProfileNotificationsPage() {
+export default function NotificationsPage() {
     return (
         <View style={mainStyles.container}>
             <Text>profile Notifications Page</Text>
