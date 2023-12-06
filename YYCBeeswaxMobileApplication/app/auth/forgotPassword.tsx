fb6ffdf201524cb52ad4c5043ec243107f8fdb62
maxwell-kepler <maxwell.kepler@ucalgary.ca>
2023-12-05 17:32:34 -0700

    Added functionality to forgotPassword

--- YYCBeeswaxMobileApplication/app/auth/forgotPassword.tsx
+++ YYCBeeswaxMobileApplication/app/auth/forgotPassword.tsx
@@ -1,20 +1,40 @@
-import {View} from 'react-native';
-import HideableInput from "@/components/hideableInput";
+import { useState } from 'react';
+import { Text, View } from 'react-native';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from "@/firebase/config";
+import { Fontisto } from '@expo/vector-icons';
 import Button from '@/components/button'
-import {accountStyles} from "@/styles/accountStyles";
 import Header from "@/components/header";
+import Input from '@/components/input';
+import { forgotPageStyles } from "@/styles/forgotPageStyles";
 
 export default function App() {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+
+    async function confirm() {
+        try {
+            await sendPasswordResetEmail(auth, email);
+            //USE NAVIGATION TO RETURN BACK TO LOGIN
+        } catch (err: any) {
+            if (err?.code == "auth/missing-email") {
+                setError('Password Reset Failed - Enter your email.');
+            } else {
+                setError(err?.code);
+            }
+        }
+    }
+
     return (
-        <View style={accountStyles.container}>
-            <Header header={'Forget Password'}/>
-            <View style={accountStyles.form}>
-                <HideableInput label='Old Password' placeholder='Enter old password'/>
-                <HideableInput label='New Password' placeholder='Enter new password'/>
-                <HideableInput label='Confirm New Password' placeholder='Re-enter new password'/>
+        <View style={forgotPageStyles.container}>
+            <Header header={'Forgot Password'}/>
+            <View style={forgotPageStyles.form}>
+                <Fontisto name="locked" size={80} color="black" style={forgotPageStyles.logo}/>
+                <Text style={forgotPageStyles.text}>Please enter your email address to receive a link to reset your password. You will then be prompted to login.</Text>
+                <Input label={'Email'} placeholder='Enter your email' value={email} onChangeText={setEmail}/>
+                {error && <Text style={forgotPageStyles.error}>{error}</Text>}
             </View>
-            <Button title={'Confirm'}/>
+            <Button title={'Confirm'} onPress={confirm}/>
         </View>
     )
 }
-
