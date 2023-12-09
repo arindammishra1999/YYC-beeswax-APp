c3ec447e9e445a08f409cce48143e5ec0b053c16
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2023-12-08 21:06:55 -0700

    updated formatting

--- YYCBeeswaxMobileApplication/styles/components/logoutPopupStyles.tsx
+++ YYCBeeswaxMobileApplication/styles/components/logoutPopupStyles.tsx
@@ -1,52 +1,51 @@
-import {StyleSheet} from 'react-native';
-import {colors} from "@/consts/styles";
+import { StyleSheet } from "react-native";
+import { colors } from "@/consts/styles";
 
 export const logoutPopupStyles = StyleSheet.create({
-      viewContainer: {
+    viewContainer: {
         flex: 1,
-        justifyContent: 'center',
-      },
-      popupView: {
+        justifyContent: "center",
+    },
+    popupView: {
         marginHorizontal: 30,
         backgroundColor: colors.white,
         borderRadius: 20,
         paddingVertical: 40,
-        alignItems: 'center',
-        shadowColor: '#000',
+        alignItems: "center",
+        shadowColor: "#000",
         shadowOpacity: 0.25,
         shadowRadius: 10,
-      },
-      popupText: {
+    },
+    popupText: {
         paddingHorizontal: 40,
         color: colors.black,
         fontSize: 16,
-        fontWeight: 'bold',
+        fontWeight: "bold",
         marginBottom: 30,
-        textAlign: 'center',
-      },
-      buttonContainer: {
+        textAlign: "center",
+    },
+    buttonContainer: {
         width: 300,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      },
-      button: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+    },
+    button: {
         borderRadius: 30,
         paddingHorizontal: 50,
         paddingVertical: 15,
         backgroundColor: colors.yellow,
-      },
-      buttonTextStyle: {
+    },
+    buttonTextStyle: {
         color: colors.black,
         fontSize: 16,
-        fontWeight: 'bold',
-      },
-      touchableOverlay: {
-        position: 'absolute',
+        fontWeight: "bold",
+    },
+    touchableOverlay: {
+        position: "absolute",
         top: 0,
         bottom: 0,
         left: 0,
         right: 0,
-    }
-
+    },
 });
