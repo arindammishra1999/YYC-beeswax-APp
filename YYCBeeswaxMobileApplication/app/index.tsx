be23615c572b3d1098924481ed4ed48fa465bc78
Jaxson19W <77870981+Jaxson19W@users.noreply.github.com>
2023-12-18 16:17:21 -0800

    created language page

--- YYCBeeswaxMobileApplication/app/index.tsx
+++ YYCBeeswaxMobileApplication/app/index.tsx
@@ -5,17 +5,14 @@ import LoginPage from "./pages/LoginPage";
 import Navbar from './components/navbar';
 import { Text, SafeAreaView } from "react-native";
 import { mainStyles } from "./styles/mainStyles";
+import LanguagePage from "./pages/LanguagePage";
 
 export default function App() {
     const { user } = useAuth();
 
     return (
         <SafeAreaView style={mainStyles.container}>
-            <LoginPage />
-            <Text>Hello World</Text>
-            <Text>{user?.uid || "Not Logged In"}</Text>
-            <StatusBar style="auto" />
-            <Navbar/>
+            <LanguagePage />
         </SafeAreaView>
     );
 }
---
+++ YYCBeeswaxMobileApplication/app/pages/LanguagePage.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import Header from "../components/header";
+import { accountStyles } from "../styles/accountStyles";
+import { headerStyles } from '../styles/headerStyles';
+import NavBar from "../components/navbar";
+import SubmitButton from "../components/submitButton";
+
+const LanguageSelectionPage = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState('English');
+
+  const availableLanguages = ['English', 'French', 'Spanish', 'German', 'Chinese'];
+
+  const handleLanguageChange = (language) => {
+    setSelectedLanguage(language);
+  };
+
+  const handleConfirmChange = () => {
+    // Add logic to confirm and save the selected language
+    console.log(`Selected language: ${selectedLanguage}`);
+  };
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Header header="Language" />
+        <View>
+          <Text style={styles.sectionHeader}>Available Languages:</Text>
+        </View>
+        <ScrollView style={styles.languageList}>
+          {availableLanguages.map((language) => (
+            <View key={language} style={styles.languageContainer}>
+              <Text style={styles.languageText}>{language}</Text>
+              <TouchableOpacity
+                style={[
+                  styles.circleButton,
+                  selectedLanguage === language ? styles.blueCircle : styles.clearCircle,
+                ]}
+                onPress={() => handleLanguageChange(language)}
+                disabled={selectedLanguage === language}
+              />
+            </View>
+          ))}
+        </ScrollView>
+        <View style={styles.bottomContainer}>
+          <SubmitButton title="Confirm Changes" handleClick={handleConfirmChange} />
+          <NavBar />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'space-between',
+  },
+  languageList: {
+    flex: 1,
+  },
+  languageContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  languageText: {
+    fontSize: 16,
+  },
+  circleButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 15, // half of width and height to create a circle
+    borderWidth: 1,
+    borderColor: '#007BFF',
+    marginLeft: 10,
+  },
+  blueCircle: {
+    backgroundColor: '#007BFF',
+  },
+  clearCircle: {
+    backgroundColor: 'transparent',
+  },
+  sectionHeader: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  bottomContainer: {
+    justifyContent: 'flex-end',
+  },
+});
+
+export default LanguageSelectionPage;
