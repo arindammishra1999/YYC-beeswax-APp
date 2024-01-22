80e9e408acc35ba6456905773b84ea4e67cc79fc
Colin-C32 <colin.christophe@ucalgary.ca>
2024-01-21 23:47:00 -0700

    retreiving orders

---
+++ YYCBeeswaxMobileApplication/firebase/getCollections/getUserOrders.tsx
@@ -0,0 +1,22 @@
+import { doc, getDocs, collection } from "firebase/firestore";
+
+import { db } from "@/firebase/config";
+
+export async function getUserOrders(id: any) {
+    try {
+        const userDocRef = doc(db, "users", id);
+
+        const orderSnapshot = await getDocs(collection(userDocRef, "orders"));
+
+        const orders = orderSnapshot.docs.map((doc) => {
+            return {
+                id: doc.id,
+                data: doc.data(),
+            };
+        });
+
+        return orders;
+    } catch (error) {
+        console.error("Error getting documents: ", error);
+    }
+}
