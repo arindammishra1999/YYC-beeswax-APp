4f1e86ad7a8b01dbc2c7d3872cd813b0d56f3feb
Jaxson19W <77870981+Jaxson19W@users.noreply.github.com>
2024-01-22 20:27:19 -0700

    Made changes according to feedback

--- YYCBeeswaxMobileApplication/app/dashboard/SearchPage.tsx
+++ YYCBeeswaxMobileApplication/app/dashboard/SearchPage.tsx
@@ -13,7 +13,6 @@ import Header from "@/components/header";
 import ProductSimpleCard from "@/components/productSimpleCard";
 import { getProductData } from "@/firebase/getCollections/getProducts";
 import { mainStyles } from "@/styles/mainStyles";
-import { profilePageStyles } from "@/styles/profilePageStyles";
 import { queryPageStyles } from "@/styles/queryPageStyles";
 
 export default function SearchPage() {
@@ -78,14 +77,14 @@ export default function SearchPage() {
         return (
             <View style={mainStyles.container}>
                 <Header header="Search Results" />
-                <Text style={profilePageStyles.messageText}>
+                <Text style={queryPageStyles.messageText}>
                     Sorry, nothing was found for {searchTerm}.
                 </Text>
                 <TouchableOpacity
-                    style={profilePageStyles.button}
+                    style={queryPageStyles.button}
                     onPress={() => router.push("/dashboard/HomePage")}
                 >
-                    <Text style={profilePageStyles.buttonText}>
+                    <Text style={queryPageStyles.buttonText}>
                         Shop Other Products
                     </Text>
                 </TouchableOpacity>
