dbb028b1262b9e804631208ce538e847d1a4b9e8
maxwell-kepler <maxwell.kepler@ucalgary.ca>
2024-01-23 16:17:43 -0700

    Updated the headers format
    Otherwise, on a large search term, the back arrow gets covered by the header's title. Now it should be more accessible.

--- YYCBeeswaxMobileApplication/styles/components/headerStyles.tsx
+++ YYCBeeswaxMobileApplication/styles/components/headerStyles.tsx
@@ -1,13 +1,19 @@
 import { StyleSheet } from "react-native";
 
+import { viewportHeight, viewportWidth } from "@/consts/viewport";
+
 export const headerStyles = StyleSheet.create({
     header: {
         flexDirection: "row",
         width: "100%",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-end",
         paddingVertical: 15,
     },
-    headerText: { fontSize: 22, fontWeight: "bold" },
+    headerText: {
+        fontSize: 22,
+        fontWeight: "bold",
+        width: "85%",
+    },
     backButton: { position: "absolute", left: 10 },
 });
