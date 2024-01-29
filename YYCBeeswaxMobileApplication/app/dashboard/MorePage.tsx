0b7d54ed479d4863b665930bb9d539e38e16aa7e
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-01-29 16:38:51 -0700

    initial addition of social media links

--- YYCBeeswaxMobileApplication/app/dashboard/MorePage.tsx
+++ YYCBeeswaxMobileApplication/app/dashboard/MorePage.tsx
@@ -1,6 +1,7 @@
 import { router } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import Header from "@/components/header";
 import MoreOption from "@/components/moreOptions";
@@ -20,6 +21,49 @@ export default function MorePage() {
                 label="Quizzes"
                 iconName="comment-question"
                 onPress={() => router.push("/dashboard/quizzes/")}
+            />
+            <MoreOption
+                label="Facebook"
+                iconName="facebook"
+                onPress={() =>
+                    WebBrowser.openBrowserAsync(
+                        "https://www.facebook.com/YYCBeeswax/",
+                    )
+                }
+            />
+            <MoreOption
+                label="Twitter"
+                iconName="twitter"
+                onPress={() =>
+                    WebBrowser.openBrowserAsync("https://twitter.com/yycwax")
+                }
+            />
+            <MoreOption
+                label="Instagram"
+                iconName="instagram"
+                onPress={() =>
+                    WebBrowser.openBrowserAsync(
+                        "https://www.instagram.com/yycwax/",
+                    )
+                }
+            />
+            <MoreOption
+                label="Pinterest"
+                iconName="pinterest"
+                onPress={() =>
+                    WebBrowser.openBrowserAsync(
+                        "https://www.pinterest.ca/yycbeeswax/",
+                    )
+                }
+            />
+            <MoreOption
+                label="Youtube"
+                iconName="youtube"
+                onPress={() =>
+                    WebBrowser.openBrowserAsync(
+                        "https://www.youtube.com/channel/UCimhrXjrRQf1a7dgiZtFzLw",
+                    )
+                }
             />
             <Navbar currentPage="More" />
         </View>
