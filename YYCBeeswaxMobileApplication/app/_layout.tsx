ed5eafaea79fa425a89a969e53e0d88222dc7bc5
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-01-31 23:14:35 -0700

    fixed nav bug

--- YYCBeeswaxMobileApplication/app/_layout.tsx
+++ YYCBeeswaxMobileApplication/app/_layout.tsx
@@ -40,6 +40,13 @@ export default function RootLayout() {
                     }}
                 >
                     <Stack.Screen
+                        name="dashboard"
+                        options={{
+                            gestureEnabled: false,
+                            animation: "none",
+                        }}
+                    />
+                    <Stack.Screen
                         name="auth/emailVerification"
                         options={{
                             gestureEnabled: false,
