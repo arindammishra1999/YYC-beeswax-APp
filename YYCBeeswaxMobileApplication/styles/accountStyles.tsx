54aa8389efa8944f368d1d1f480bf3ca17cce001
Colin-C32 <colin.christophe@ucalgary.ca>
2024-02-10 18:20:07 -0700

    updating padding

--- YYCBeeswaxMobileApplication/styles/accountStyles.tsx
+++ YYCBeeswaxMobileApplication/styles/accountStyles.tsx
@@ -10,7 +10,14 @@ export const accountStyles = StyleSheet.create({
         alignContent: "space-between",
         backgroundColor: colors.white,
     },
-    formContainer: { backgroundColor: colors.white, gap: 30, flexGrow: 1 },
+    formContainer: {
+        backgroundColor: colors.white,
+        gap: 30,
+        flexGrow: 1,
+        addingHorizontal: 10,
+        paddingBottom: 10,
+        alignContent: "space-between",
+    },
     form: { flex: 1, paddingTop: 10, gap: 20, paddingHorizontal: 10 },
     logo: {
         width: 80,
