dee6f0766971a432dec6df22db6809c6f7d70924
Kaumil Patel <79810862+Kal0511@users.noreply.github.com>
2024-02-17 23:45:04 -0700

    Update loginWithGoogle.ts

--- YYCBeeswaxMobileApplication/firebase/hooks/loginWithGoogle.ts
+++ YYCBeeswaxMobileApplication/firebase/hooks/loginWithGoogle.ts
@@ -1,15 +1,21 @@
-import { GoogleAuthProvider, OAuthCredential, signInWithCredential, UserCredential } from "@firebase/auth";
+import {
+    GoogleAuthProvider,
+    OAuthCredential,
+    signInWithCredential,
+    UserCredential,
+} from "@firebase/auth";
 import { useIdTokenAuthRequest } from "expo-auth-session/build/providers/Google";
 import { useCallback, useEffect } from "react";
 
 import { auth } from "@/firebase/config";
+import { setUser } from "@/firebase/setCollections/setUser";
 
 export function useLoginWithGoogle() {
     const [, authSessionResult, promptGoogle] = useIdTokenAuthRequest({
         selectAccount: true,
         clientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
         androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
-        iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID
+        iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
     });
 
     // Handles the login via the Google Provider
@@ -20,19 +26,28 @@ export function useLoginWithGoogle() {
     // Function that logs into firebase using the credentials from an OAuth provider
     const loginToFirebase = useCallback(
         async (credentials: OAuthCredential) => {
-            const signInResponse = await signInWithCredential(auth, credentials) as UserCredential & {
-                _tokenResponse: { isNewUser?: boolean }
+            const signInResponse = (await signInWithCredential(
+                auth,
+                credentials,
+            )) as UserCredential & {
+                _tokenResponse: { isNewUser?: boolean };
             };
-            console.log(signInResponse);
+            if (signInResponse._tokenResponse.isNewUser) {
+                await setUser(signInResponse.user.uid, {
+                    email: signInResponse.user.email,
+                    name: signInResponse.user.displayName,
+                });
+            }
             console.log(signInResponse._tokenResponse.isNewUser);
+            console.log(signInResponse);
         },
-        []
+        [],
     );
 
     useEffect(() => {
         if (authSessionResult?.type === "success") {
             const credentials = GoogleAuthProvider.credential(
-                authSessionResult.params.id_token
+                authSessionResult.params.id_token,
             );
             loginToFirebase(credentials);
         }
